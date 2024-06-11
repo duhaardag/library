a29139bd3dbe9d9ed2384df5ad9734c74c27665c
duhaardag <ardagduha4@gmail.com>
2024-06-11 23:52:18 +0200

    create patron_1 object on main

---
+++ main.py
@@ -0,0 +1,5 @@
+from patron import Patron
+
+patron_1 = Patron('Duha', 18, 'Male', '34496')
+
+print(patron_1.age)
--- patron.py
+++ patron.py
@@ -1,5 +1,6 @@
 from person import Person
 
 class Patron(Person):
-    def init(self, patron_id):
+    def __init__(self, name, age, gender, patron_id):
+        super().__init__(name, age, gender) 
         self.patron_id = patron_id
